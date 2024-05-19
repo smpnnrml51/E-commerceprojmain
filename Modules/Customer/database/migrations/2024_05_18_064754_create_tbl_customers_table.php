36d0c9b1296748bfe7a7fa45f69fc5d0f81e10a4
Nabin Bhattarai <92159325+Nabinbht@users.noreply.github.com>
2024-05-19 23:08:03 +0545

    update

--- Modules/Customer/database/migrations/2024_05_18_064754_create_tbl_customers_table.php
+++ Modules/Customer/database/migrations/2024_05_18_064754_create_tbl_customers_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('tbl_customers', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('contact');
+            $table->text('address');
             $table->timestamps();
         });
     }
