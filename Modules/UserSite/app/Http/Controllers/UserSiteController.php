036b737b1820b012a90bff0035370b0a4c796f91
Subodh Shrestha <shresthasubodh73@gmail.com>
2024-05-28 16:03:59 +0545

    customer home page and customer layout

--- Modules/UserSite/app/Http/Controllers/UserSiteController.php
+++ Modules/UserSite/app/Http/Controllers/UserSiteController.php
@@ -14,7 +14,7 @@ class UserSiteController extends Controller
      */
     public function index()
     {
-        return view('usersite::index');
+        return view('usersite::home.index');
     }
 
     /**
